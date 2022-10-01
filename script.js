38d856c77e9be7699774da5f8f4c43ce6eff6ed6
Jasper Huting <jasper.huting@censo.nl>
2022-10-01 15:19:18 +0200

    automaticly images from folder

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 $(document).ready(function () {
-  $(".your-class").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    navigation: false,
-    fade: true,
-    cssEase: "linear",
-    arrows: false,
-    centerMode: true,
-    infinite: true,
-    adaptive: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
+  var folder = "images/";
+
+  $.ajax({
+    url: folder,
+    success: function (data) {
+      $(data)
+        .find("a")
+        .attr("href", function (i, val) {
+          if (val.match(/\.(jpe?g|png|gif)$/)) {
+            $(".your-class").append(
+              "<div class='slide'><img src='" + folder + val + "'></div>"
+            );
+          }
+        });
+    },
   });
+
+  setTimeout(() => {
+    $(".your-class").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      navigation: false,
+      fade: true,
+      cssEase: "linear",
+      arrows: false,
+      centerMode: true,
+      infinite: true,
+      adaptive: true,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+    });
+  }, 5000);
 });
