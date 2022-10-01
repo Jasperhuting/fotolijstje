e22be75af607023871c3a73d2de0ddb27ec6f2f7
Jasper Huting <jasper.huting@censo.nl>
2022-10-01 15:02:39 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".your-class").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    navigation: false,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    centerMode: true,
+    infinite: true,
+    adaptive: true,
+  });
+});
